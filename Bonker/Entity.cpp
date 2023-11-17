1fbc5ec68cc1bbf5c77b1920d189f073298f0734
Jaden-Thakur <70930877+Jaden-Thakur@users.noreply.github.com>
2023-11-17 13:42:47 -0500

    Basic Setup

---
+++ Bonker/Entity.cpp
@@ -0,0 +1,273 @@
+﻿/**
+* Author: Jaden Thakur
+* Assignment: Rise of the AI
+* Date due: 2023-11-18, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+#define GL_SILENCE_DEPRECATION
+#define STB_IMAGE_IMPLEMENTATION
+#define LOG(argument) std::cout << argument << std::endl;
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+#include "Entity.h"
+
+
+const float GRAVITY = 1.0f;
+const float AIR_RESISTANCE = 0.5f;
+const float max_accel = 1.0f;
+
+Entity::Entity() {
+    // PHYSICS (GRAVITY) 
+    m_position = glm::vec3(0.0f);
+    m_velocity = glm::vec3(0.0f);
+    m_acceleration = glm::vec3(0.0f);
+
+    // TRANSFORMATIONS
+    m_movement = glm::vec3(0.0f);
+    m_speed = 0.0;
+    m_angle = 0.0;
+    m_model_matrix = glm::mat4(1.0f);
+}
+
+Entity::~Entity() {
+    delete[] m_animation;
+};
+
+void Entity::rotate(float angle) {
+    //if ((m_angle + angle <= 90) || (m_angle + angle >= 270 && m_angle + angle <= 359)) {
+    this->m_model_matrix = glm::rotate(this->m_model_matrix, glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
+    m_angle += angle;
+    LOG(angle);
+    //} 
+};
+
+void Entity::scale() {
+    // scales model to current height and width, yes you have to set the height and width first Jaden was lazy
+    m_model_matrix = glm::scale(m_model_matrix, glm::vec3(m_width, m_height, 1.0));
+}
+
+void Entity::accelerate(float delta_time) {
+    float amount_x = 0;
+    float amount_y = 0;
+
+    // choosing direction of acceleration and accelerating in that direction
+    if (0 <= m_angle && m_angle <= 90) {
+        amount_x = (m_angle / 90);
+        amount_y = 1 - amount_x;
+        m_acceleration.x -= amount_x * m_acceleration_rate;
+        m_acceleration.y += amount_y * m_acceleration_rate;
+    }
+    else if (90 < m_angle && m_angle <= 180) {
+        amount_y = ((m_angle - 90) / 90);
+        amount_x = 1 - amount_y;
+        m_acceleration.x -= amount_x * m_acceleration_rate;
+        m_acceleration.y -= amount_y * m_acceleration_rate;
+    }
+    else if (180 < m_angle && m_angle <= 270) {
+        amount_x = ((m_angle - 180) / 90);
+        amount_y = 1 - amount_x;
+        m_acceleration.x += amount_x * m_acceleration_rate;
+        m_acceleration.y -= amount_y * m_acceleration_rate;
+    }
+    else if (270 < m_angle && m_angle <= 359) {
+        amount_y = ((m_angle - 270) / 90);
+        amount_x = 1 - amount_y;
+        m_acceleration.x += amount_x * m_acceleration_rate;
+        m_acceleration.y += amount_y * m_acceleration_rate;
+    }
+
+    if (m_velocity.x > max_accel) {
+        m_velocity.x = max_accel;
+    }
+
+    if (m_velocity.y > max_accel) {
+        m_velocity.y = max_accel;
+    }
+
+
+};
+
+void Entity::activate() {
+    this->m_is_active = true;
+};
+void Entity::deactivate() {
+    this->m_is_active = false;
+};
+
+
+void Entity::update(float delta_time, Entity* player, Entity* collidable_entities1, int collidable_entity_count)
+{
+    if (!m_is_active) return;
+
+    // collision checks
+    m_collided_top = false;
+    m_collided_bottom = false;
+    m_collided_left = false;
+    m_collided_right = false;
+
+
+    // gravity
+    m_velocity += m_acceleration * delta_time;
+    m_velocity.y -= GRAVITY * delta_time;
+
+
+
+    m_position.y += m_velocity.y * delta_time;
+    check_collision_y(collidable_entities1, collidable_entity_count);
+
+    m_position.x += m_velocity.x * delta_time;
+    check_collision_x(collidable_entities1, collidable_entity_count);
+
+    if (m_collided_bottom) {
+        m_acceleration.x = 0;
+    }
+
+
+    // ����� TRANSFORMATIONS ����� //
+    m_model_matrix = glm::mat4(1.0f);
+    m_model_matrix = glm::translate(m_model_matrix, m_position);
+}
+
+void const Entity::check_collision_y(Entity* collidable_entities, int collidable_entity_count)
+{
+    for (int i = 0; i < collidable_entity_count; i++)
+    {
+        Entity* collidable_entity = &collidable_entities[i];
+
+        if (check_collision(collidable_entity))
+        {
+            float y_distance = fabs(m_position.y - collidable_entity->get_position().y);
+            float y_overlap = fabs(y_distance - (m_height / 2.0f) - (collidable_entity->get_height() / 2.0f));
+            if (m_velocity.y > 0) {
+                m_position.y -= y_overlap;
+                m_velocity.y = 0;
+                m_collided_top = true;
+            }
+            else if (m_velocity.y < 0) {
+                m_position.y += y_overlap;
+                m_velocity.y = 0;
+                m_collided_bottom = true;
+
+            }
+
+        }
+
+
+    }
+}
+
+void const Entity::check_collision_x(Entity* collidable_entities, int collidable_entity_count)
+{
+    for (int i = 0; i < collidable_entity_count; i++)
+    {
+        Entity* collidable_entity = &collidable_entities[i];
+
+        if (check_collision(collidable_entity))
+        {
+            float x_distance = fabs(m_position.x - collidable_entity->get_position().x);
+            float x_overlap = fabs(x_distance - (m_width / 2.0f) - (collidable_entity->get_width() / 2.0f));
+            if (m_velocity.x > 0) {
+                m_position.x -= x_overlap;
+                m_velocity.x = 0;
+                m_collided_right = true;
+            }
+            else if (m_velocity.x < 0) {
+                m_position.x += x_overlap;
+                m_velocity.x = 0;
+                m_collided_left = true;
+            }
+
+        }
+    }
+}
+
+void Entity::render(ShaderProgram* program)
+{
+    program->set_model_matrix(m_model_matrix);
+
+    if (!m_is_active) { return; }
+
+    if (m_animation_indices != NULL)
+    {
+        draw_sprite_from_texture_atlas(program, m_texture_id, m_animation_indices[m_animation_index]);
+        return;
+    }
+
+    float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float tex_coords[] = { 0.0,  1.0, 1.0,  1.0, 1.0, 0.0,  0.0,  1.0, 1.0, 0.0,  0.0, 0.0 };
+
+    glBindTexture(GL_TEXTURE_2D, m_texture_id);
+
+    glVertexAttribPointer(program->get_position_attribute(), 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program->get_position_attribute());
+    glVertexAttribPointer(program->get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, tex_coords);
+    glEnableVertexAttribArray(program->get_tex_coordinate_attribute());
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glDisableVertexAttribArray(program->get_position_attribute());
+    glDisableVertexAttribArray(program->get_tex_coordinate_attribute());
+}
+
+bool const Entity::check_collision(Entity* other) const
+{
+    if (other == this) return false;
+    // If either entity is inactive, there shouldn't be any collision
+    if (!m_is_active || !other->m_is_active) return false;
+
+    float x_distance = fabs(m_position.x - other->m_position.x) - ((m_width + other->m_width) / 2.0f);
+    float y_distance = fabs(m_position.y - other->m_position.y) - ((m_height + other->m_height) / 2.0f);
+
+    return x_distance < 0.0f && y_distance < 0.0f;
+}
+
+void Entity::draw_sprite_from_texture_atlas(ShaderProgram* program, GLuint texture_id, int index)
+{
+    // Step 1: Calculate the UV location of the indexed frame
+    float u_coord = (float)(index % m_animation_cols) / (float)m_animation_cols;
+    float v_coord = (float)(index / m_animation_cols) / (float)m_animation_rows;
+
+    // Step 2: Calculate its UV size
+    float width = 1.0f / (float)m_animation_cols;
+    float height = 1.0f / (float)m_animation_rows;
+
+    // Step 3: Just as we have done before, match the texture coordinates to the vertices
+    float tex_coords[] =
+    {
+        u_coord, v_coord + height, u_coord + width, v_coord + height, u_coord + width, v_coord,
+        u_coord, v_coord + height, u_coord + width, v_coord, u_coord, v_coord
+    };
+
+    float vertices[] =
+    {
+        -0.5, -0.5, 0.5, -0.5,  0.5, 0.5,
+        -0.5, -0.5, 0.5,  0.5, -0.5, 0.5
+    };
+
+    // Step 4: And render
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+
+    glVertexAttribPointer(program->get_position_attribute(), 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program->get_position_attribute());
+
+    glVertexAttribPointer(program->get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, tex_coords);
+    glEnableVertexAttribArray(program->get_tex_coordinate_attribute());
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glDisableVertexAttribArray(program->get_position_attribute());
+    glDisableVertexAttribArray(program->get_tex_coordinate_attribute());
+}
---
+++ Bonker/main.cpp
@@ -0,0 +1,376 @@
+/**
+* Author: Jaden Thakur
+* Assignment: Lunar Lander
+* Date due: 2023-11-08, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+// #defines
+#define GL_SILENCE_DEPRECATION
+#define GL_GLEXT_PROTOTYPES 1
+#define LOG(argument) std::cout << argument << std::endl;
+#define STB_IMAGE_IMPLEMENTATION
+#define FIXED_TIMESTEP 0.0166666f
+#define LANDZONE_COUNT 5
+#define PLATFORM_COUNT 222 - LANDZONE_COUNT
+
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+// includes
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+#include "stb_image.h"
+#include <vector>
+#include "Entity.h"
+#include "Map.h"
+#include "SDL_mixer.h"
+#include "cmath"
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+
+
+
+struct GameState
+{
+    Map* map;
+    Entity* player;
+    Entity* ui;
+    Entity* enemies;
+};
+
+// Globals 
+const int WINDOW_WIDTH = 1000,
+WINDOW_HEIGHT = 600;
+
+const float BG_RED = 0.0f,
+BG_BLUE = 0.1f,
+BG_GREEN = 0.0f,
+BG_OPACITY = 1.0f;
+
+const int VIEWPORT_X = 0,
+VIEWPORT_Y = 0,
+VIEWPORT_WIDTH = WINDOW_WIDTH,
+VIEWPORT_HEIGHT = WINDOW_HEIGHT;
+
+const char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
+F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
+
+const float MILLISECONDS_IN_SECOND = 1000.0;
+const char SPRITESHEET_FILEPATH[] = "assets/Ship_Sprite_Sheet_2.png";
+const char PLATFORM_FILEPATH[] = "assets/platformPack_tile027.png";
+const char LANDZONE_FILEPATH[] = "assets/platformPack_tile028.png";
+const char WIN_SCREEN[] = "assets/win.png";
+const char LOSE_SCREEN[] = "assets/game_over.png";
+
+const int NUMBER_OF_TEXTURES = 1;
+const GLint LEVEL_OF_DETAIL = 0;
+const GLint TEXTURE_BORDER = 0;
+
+
+GameState g_state;
+
+SDL_Window* g_display_window;
+bool go = false;
+bool win = false;
+bool g_game_is_running = true;
+
+ShaderProgram g_program;
+glm::mat4 g_view_matrix, g_projection_matrix;
+
+float g_previous_ticks = 0.0f;
+float g_accumulator = 0.0f;
+int randint;
+
+void initialise()
+{
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+    g_display_window = SDL_CreateWindow("Lunar Lander Clone",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_OPENGL);
+
+    SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
+    SDL_GL_MakeCurrent(g_display_window, context);
+
+#ifdef _WINDOWS
+    glewInit();
+#endif
+
+    // Viewport Setup 
+    glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+
+    g_program.load(V_SHADER_PATH, F_SHADER_PATH);
+
+    g_view_matrix = glm::mat4(1.0f);
+    g_projection_matrix = glm::ortho(-15.0f, 15.0f, -10.0f, 10.0f, -1.0f, 1.0f);
+
+    g_program.set_projection_matrix(g_projection_matrix);
+    g_program.set_view_matrix(g_view_matrix);
+
+    glUseProgram(g_program.get_program_id());
+
+    glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
+
+
+    // Platforms and Landing Zones
+    GLuint platform_texture_id = Utility::load_texture(PLATFORM_FILEPATH);
+    GLuint landzone_texture_id = load_texture(LANDZONE_FILEPATH);
+
+    g_state.platforms = new Entity[PLATFORM_COUNT];
+    g_state.landzones = new Entity[LANDZONE_COUNT];
+
+
+    std::vector<int> platform_heights = { 11, 10, 9, 8, 7, 7, 8, 7, 7, 6, 6, 5, 5, 6, 6, 6, 7, 7, 8, 9, 10, 10, 9, 7, 5, 5, 6, 6, 7, 7, 5 };
+    int platform_count = 0;
+    int landzone_counter = 0;
+
+    std::vector<int> landzone_locations;
+    srand(time(0));
+    for (int i = 0; i < LANDZONE_COUNT; i++) {
+        randint = std::rand() % 31;
+        if (std::find(landzone_locations.begin(), landzone_locations.end(), randint) == landzone_locations.end()) {
+            landzone_locations.emplace_back(randint);
+        }
+        else {
+            i--;
+        }
+
+    }
+
+    // Create platforms
+    for (int j = 0; j < 31; j++) {
+        for (int i = 0; i < platform_heights[j]; i++)
+        {
+            bool found = std::find(landzone_locations.begin(), landzone_locations.end(), j) == landzone_locations.end();
+            if (landzone_counter != LANDZONE_COUNT && (i == platform_heights[j] - 1) && found) {
+                g_state.landzones[landzone_counter].m_texture_id = landzone_texture_id;
+                g_state.landzones[landzone_counter].set_position(glm::vec3(j - 15.0f, i - 10.0f, 0.0f));
+                g_state.landzones[landzone_counter].set_width(1.0f);
+                g_state.landzones[landzone_counter].scale();
+                g_state.landzones[landzone_counter].set_entity_type(LANDZONE);
+                g_state.landzones[landzone_counter].update(0.0f, g_state.player, NULL, NULL, 0);
+                landzone_counter++;
+            }
+            else {
+                g_state.platforms[platform_count].m_texture_id = platform_texture_id;
+                g_state.platforms[platform_count].set_entity_type(PLATFORM);
+                g_state.platforms[platform_count].set_position(glm::vec3(j - 15.0f, i - 10.0f, 0.0f));
+                g_state.platforms[platform_count].set_width(1.0f);
+                g_state.platforms[platform_count].scale();
+                g_state.platforms[platform_count].update(0.0f, g_state.player, NULL, NULL, 0);
+                platform_count++;
+            }
+        }
+    }
+
+
+    // Player Stuff
+    // Create Player
+    g_state.player = new Entity();
+    g_state.player->set_position(glm::vec3(-14.0f, 9.0f, 0.0f));
+    g_state.player->set_movement(glm::vec3(0.0f));
+    g_state.player->set_speed(1.0f);
+    g_state.player->set_acceleration(glm::vec3(0.0f, -4.905f, 0.0f));
+    g_state.player->m_texture_id = load_texture(SPRITESHEET_FILEPATH);
+
+    // Flying
+    g_state.player->m_animation[0] = new int[1] {0};
+    g_state.player->m_animation[1] = new int[1] {1};
+    g_state.player->m_acceleration_rate = 0.75f;
+    g_state.player->set_entity_type(PLAYER);
+    g_state.player->m_animation_index = 0;
+    g_state.player->m_animation_cols = 2;
+    g_state.player->m_animation_rows = 1;
+    g_state.player->m_animation_indices = g_state.player->m_animation[0];
+
+    // UI
+    g_state.ui = new Entity();
+    g_state.ui->set_entity_type(UI);
+    g_state.ui->set_position(glm::vec3(0.0f, 0.0f, 0.0f));
+    g_state.ui->set_width(12.0f);
+    g_state.ui->set_height(6.0f);
+    g_state.ui->scale();
+    g_state.ui->deactivate();
+
+    // Needed stuff
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void process_input()
+{
+
+    // Stop player from moving without input
+    g_state.player->set_movement(glm::vec3(0.0f));
+
+    // One Click Events
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type) {
+            // End game
+        case SDL_QUIT:
+        case SDL_WINDOWEVENT_CLOSE:
+            g_game_is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+            case SDLK_ESCAPE:
+                // Quit
+                g_game_is_running = false;
+                break;
+            case SDLK_SPACE:
+                // Accelerate
+                g_state.player->m_is_accelerating = true;
+                break;
+            default:
+                break;
+            }
+        default:
+            break;
+        }
+    }
+
+
+    // Holding Down Keys
+    const Uint8* key_state = SDL_GetKeyboardState(NULL);
+
+    if (key_state[SDL_SCANCODE_A])
+    {
+        g_state.player->rotate(0.025);
+    }
+    if (key_state[SDL_SCANCODE_D])
+    {
+        g_state.player->rotate(-0.025);
+    }
+    if (key_state[SDL_SCANCODE_SPACE]) {
+        g_state.player->m_animation_indices = g_state.player->m_animation[1];
+        g_state.player->m_is_accelerating = true;
+    }
+    else if (!key_state[SDL_SCANCODE_SPACE]) {
+        g_state.player->m_animation_indices = g_state.player->m_animation[0];
+    }
+
+    // normalize movement
+    if (glm::length(g_state.player->get_movement()) > 1.0f)
+    {
+        g_state.player->set_movement(
+            glm::normalize(
+                g_state.player->get_movement()
+            )
+        );
+    }
+}
+
+void update()
+{
+    // delta time
+    float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND;
+    float delta_time = ticks - g_previous_ticks;
+    g_previous_ticks = ticks;
+
+    delta_time += g_accumulator;
+
+    if (delta_time < FIXED_TIMESTEP)
+    {
+        g_accumulator = delta_time;
+        return;
+    }
+
+    while (delta_time >= FIXED_TIMESTEP)
+    {
+        g_state.player->update(FIXED_TIMESTEP, g_state.player, g_state.platforms, g_state.landzones, PLATFORM_COUNT + LANDZONE_COUNT);
+        delta_time -= FIXED_TIMESTEP;
+    }
+
+    g_accumulator = delta_time;
+
+
+    // Win and Lose States
+    if (g_state.player->m_landed) {
+        if (g_state.player->m_collided_bottom) {
+            go = true;
+            win = true;
+            g_state.player->m_landed = false;
+        }
+        else {
+            g_state.player->m_landed = false;
+            g_state.player->m_crashed = true;
+        }
+    }
+    else if (g_state.player->m_crashed) {
+        go = true;
+        g_state.player->m_crashed = false;
+    }
+
+
+}
+
+void render()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // deactivate all of the platforms and player on game over
+    if (go) {
+        g_state.player->deactivate();
+        for (int i = 0; i < PLATFORM_COUNT; i++) g_state.platforms[i].deactivate();
+        for (int i = 0; i < LANDZONE_COUNT; i++) g_state.landzones[i].deactivate();
+    }
+
+    // render everything except UI
+    g_state.player->render(&g_program);
+
+    for (int i = 0; i < PLATFORM_COUNT; i++) g_state.platforms[i].render(&g_program);
+    for (int i = 0; i < LANDZONE_COUNT; i++) g_state.landzones[i].render(&g_program);
+
+    // render UI elements based on win or lose
+    if (win && go) {
+        g_state.ui->activate();
+        g_state.ui->m_texture_id = load_texture(WIN_SCREEN);
+        g_state.ui->render(&g_program);
+    }
+    else if (go && !win) {
+        g_state.ui->activate();
+        g_state.ui->m_texture_id = load_texture(LOSE_SCREEN);
+        g_state.ui->render(&g_program);
+    }
+
+    // Swap window
+    SDL_GL_SwapWindow(g_display_window);
+}
+
+void shutdown()
+{
+    SDL_Quit();
+
+    // delete everything 
+    delete[] g_state.enemies;
+    delete[] g_state.platforms;
+    delete g_state.player;
+}
+
+// GAME LOOP
+int main(int argc, char* argv[])
+{
+    initialise();
+
+    while (g_game_is_running)
+    {
+        process_input();
+        update();
+        render();
+    }
+
+    shutdown();
+    return 0;
+}
